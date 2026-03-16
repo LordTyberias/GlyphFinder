c9ce5429de7765b0a9be66286ca1a20419edb8bb
Benjamin Strecker <benjamin@strecker.link>
2026-03-16 12:02:07 +0100

    Added correct Achievment IDs to TWW & Midnight Glyphs

--- Glyph_Coords/Midnight-Glyphs.lua
+++ Glyph_Coords/Midnight-Glyphs.lua
@@ -2,53 +2,53 @@
 local MIDNIGHT_GLYPH_DATA = {
     --X coord, Y coord, achievement ID, map ID ~~ /dump C_Map.GetBestMapForUnit("player")
     { -- Eversong Woods
-        {65.23, 32.63, 0, 2395}, -- Brightwing Estate
-        {33.45, 65.27, 0, 2395}, -- Daggerspine Point
-        {62.61, 62.80, 0, 2395}, -- Dawnstar Spire
-        {43.19, 46.42, 0, 2395}, -- Fairbreeze Village
-        {39.96, 59.63, 0, 2395}, -- Goldenmist Village
-        {49.46, 48.05, 0, 2395}, -- Path of Dawn
-        {58.92, 19.50, 0, 2395}, -- Silvermoon City
-        {58.43, 58.31, 0, 2395}, -- Suncrown Tree
-        {39.45, 45.63, 0, 2395}, -- Sunsail Anchorage
-        {48.35, 6.64, 0, 2393}, -- The Shining Span
-        {52.49, 67.62, 0, 2395} -- Tranquillien
+        {65.23, 32.63, 61522, 2395}, -- Brightwing Estate
+        {33.45, 65.27, 61525, 2395}, -- Daggerspine Point
+        {62.61, 62.80, 61529, 2395}, -- Dawnstar Spire
+        {43.19, 46.42, 61524, 2395}, -- Fairbreeze Village
+        {39.96, 59.63, 61527, 2395}, -- Goldenmist Village
+        {49.46, 48.05, 61531, 2395}, -- Path of Dawn
+        {58.92, 19.50, 61523, 2395}, -- Silvermoon City
+        {58.43, 58.31, 61526, 2395}, -- Suncrown Tree
+        {39.45, 45.63, 61530, 2395}, -- Sunsail Anchorage
+        {48.35, 6.64, 61521, 2393}, -- The Shining Span
+        {52.49, 67.62, 61528, 2395} -- Tranquillien
     },
     { -- Zul'Aman
-        {63.78, 81.85, 0, 2395}, -- Amani Pass
-        {30.45, 84.72, 0, 2437}, -- Nalorakk's Prowl
-        {19.22, 70.74, 0, 2437}, -- Revantusk Sedge
-        {42.94, 34.36, 0, 2437}, -- Shadebasin Watch
-        {42.75, 80.19, 0, 2437}, -- Spiritpaw Burrow
-        {46.69, 82.23, 0, 2437}, -- Solemn Valley
-        {53.21, 54.50, 0, 2437}, -- Strait of Hexx'alor
-        {53.61, 80.39, 0, 2437}, -- Temple of Akil'zon
-        {51.49, 23.56, 0, 2437}, -- Temple of Jan'alai
-        {39.54, 19.72, 0, 2437}, -- Witherbark Bluffs
-        {27.91, 28.63, 0, 2437} -- Zeb'Alar Lumberyard
+        {63.78, 81.85, 61540, 2395}, -- Amani Pass
+        {30.45, 84.72, 61537, 2437}, -- Nalorakk's Prowl
+        {19.22, 70.74, 61534, 2437}, -- Revantusk Sedge
+        {42.94, 34.36, 61532, 2437}, -- Shadebasin Watch
+        {42.75, 80.19, 61541, 2437}, -- Spiritpaw Burrow
+        {46.69, 82.23, 61539, 2437}, -- Solemn Valley
+        {53.21, 54.50, 61535, 2437}, -- Strait of Hexx'alor
+        {53.61, 80.39, 61533, 2437}, -- Temple of Akil'zon
+        {51.49, 23.56, 61542, 2437}, -- Temple of Jan'alai
+        {39.54, 19.72, 61536, 2437}, -- Witherbark Bluffs
+        {27.91, 28.63, 61538, 2437} -- Zeb'Alar Lumberyard
     },
     { -- Harandar
-        {54.74, 35.35, 0, 2413}, -- Blooming Lattice
-        {60.24, 44.41, 0, 2413}, -- Blossoming Terrace
-        {44.56, 62.86, 0, 2413}, -- Fungara Village
-        {61.86, 67.50, 0, 2413}, -- Rift of Aln
-        {69.36, 45.84, 0, 2413}, -- Roots of Amirdrassil
-        {73.08, 25.89, 0, 2413}, -- Roots of Nordrassil
-        {26.53, 61.39, 0, 2413}, -- Roots of Shaldrassil
-        {34.58, 23.13, 0, 2413} -- Roots of Teldrassil
+        {54.74, 35.35, 61547, 2413}, -- Blooming Lattice
+        {60.24, 44.41, 61543, 2413}, -- Blossoming Terrace
+        {44.56, 62.86, 61549, 2413}, -- Fungara Village
+        {61.86, 67.50, 61551, 2413}, -- Rift of Aln
+        {69.36, 45.84, 61546, 2413}, -- Roots of Amirdrassil
+        {73.08, 25.89, 61548, 2413}, -- Roots of Nordrassil
+        {26.53, 61.39, 61550, 2413}, -- Roots of Shaldrassil
+        {34.58, 23.13, 61545, 2413} -- Roots of Teldrassil
     },
     { -- Voidstorm
-        {38.91, 76.11, 0, 2405}, -- Ethereum Refinery
-        {54.95, 45.54, 0, 2405}, -- Gnawing Reach
-        {36.28, 45.01, 0, 2444}, -- Hanaar Outpost
-        {45.29, 52.27, 0, 2405}, -- Master's Perch
-        {65.08, 71.93, 0, 2405}, -- Obscurion Citadel
-        {36.08, 37.30, 0, 2405}, -- Shadowguard Point
-        {39.90, 70.98, 0, 2405}, -- The Bladeburrows
-        {49.25, 87.48, 0, 2405}, -- The Gorging Pit
-        {35.67, 61.09, 0, 2405}, -- The Ingress
-        {37.16, 49.96, 0, 2405}, -- The Molt
-        {51.32, 62.77, 0, 2405} -- The Voidspire
+        {38.91, 76.11, 61563, 2405}, -- Ethereum Refinery
+        {54.95, 45.54, 61561, 2405}, -- Gnawing Reach
+        {36.28, 45.01, 61562, 2444}, -- Hanaar Outpost
+        {45.29, 52.27, 61564, 2405}, -- Master's Perch
+        {65.08, 71.93, 61565, 2405}, -- Obscurion Citadel
+        {36.08, 37.30, 61566, 2405}, -- Shadowguard Point
+        {39.90, 70.98, 61560, 2405}, -- The Bladeburrows
+        {49.25, 87.48, 61567, 2405}, -- The Gorging Pit
+        {35.67, 61.09, 61559, 2405}, -- The Ingress
+        {37.16, 49.96, 61558, 2405}, -- The Molt
+        {51.32, 62.77, 61557, 2405} -- The Voidspire
     }
 }
 
--- Glyph_Coords/TWW-Glyphs.lua
+++ Glyph_Coords/TWW-Glyphs.lua
@@ -2,62 +2,62 @@
 local TWW_GLYPH_DATA = {
     --X coord, Y coord, achievement ID, map ID ~~ /dump C_Map.GetBestMapForUnit("player")
     { -- K'aresh
-        {60.9, 38.7, 0, 2371}, -- Castigaar
-        {46.4, 58.4, 0, 2371}, -- Eco-Dome Primus
-        {54.6, 23.3, 0, 2371}, -- Fracture of Laacuna
-        {76.4, 46.3, 0, 2371}, -- North Sufaad
-        {54.7, 53.1, 0, 2371}, -- Serrated Peaks
-        {43.9, 17.0, 0, 2371}, -- Shadow Point
-        {55.1, 67.5, 0, 2472}, -- Tazavesh
-        {74.0, 32.5, 0, 2371} -- The Oasis
+        {60.9, 38.7, 42717, 2371}, -- Castigaar
+        {46.4, 58.4, 42720, 2371}, -- Eco-Dome Primus
+        {54.6, 23.3, 42718, 2371}, -- Fracture of Laacuna
+        {76.4, 46.3, 42722, 2371}, -- North Sufaad
+        {54.7, 53.1, 42719, 2371}, -- Serrated Peaks
+        {43.9, 17.0, 42723, 2371}, -- Shadow Point
+        {55.1, 67.5, 42724, 2472}, -- Tazavesh
+        {74.0, 32.5, 42721, 2371} -- The Oasis
     },
     { -- Isle of Dorn
-        {78.2, 42.7, 0, 2248}, -- Cinderbrew Meadery
-        {44.4, 79.8, 0, 2248}, -- Dhar Durgaz
-        {23.1, 58.5, 0, 2248}, -- Dhar Oztan
-        {71.8, 47.2, 0, 2248}, -- Ironwold
-        {62.1, 44.9, 0, 2248}, -- Mourning Rise
-        {37.9, 40.9, 0, 2248}, -- Storm's Watch
-        {68.2, 71.7, 0, 2248}, -- Sunken Shield
-        {75.7, 22.2, 0, 2248}, -- The Three Shields
-        {47.8, 26.9, 0, 2248}, -- Thul Medran
-        {56.2, 17.7, 0, 2248} -- Thunderhead Peak
+        {78.2, 42.7, 40669, 2248}, -- Cinderbrew Meadery
+        {44.4, 79.8, 40665, 2248}, -- Dhar Durgaz
+        {23.1, 58.5, 40663, 2248}, -- Dhar Oztan
+        {71.8, 47.2, 40671, 2248}, -- Ironwold
+        {62.1, 44.9, 40670, 2248}, -- Mourning Rise
+        {37.9, 40.9, 40664, 2248}, -- Storm's Watch
+        {68.2, 71.7, 40666, 2248}, -- Sunken Shield
+        {75.7, 22.2, 40152, 2248}, -- The Three Shields
+        {47.8, 26.9, 40667, 2248}, -- Thul Medran
+        {56.2, 17.7, 40668, 2248} -- Thunderhead Peak
     },
     { -- The Ringing Deeps
-        {63.8, 95.0, 0, 2214}, -- Abyssal Excavation
-        {69.4, 34.4, 0, 2214}, -- Chittering Den
-        {49.0, 31.5, 0, 2214}, -- Gundargaz
-        {57.1, 31.3, 0, 2214}, -- Lost Mines
-        {62.9, 66.0, 0, 2214}, -- Taelloch Mine
-        {49.2, 66.2, 0, 2214}, -- The Living Grotto
-        {56.1, 56.0, 0, 2214}, -- The Rumbling Wastes
-        {46.9, 10.0, 0, 2214}, -- The Stonevault Exterior
-        {46.4, 51.6, 0, 2214} -- The Waterworks
+        {63.8, 95.0, 40678, 2214}, -- Abyssal Excavation
+        {69.4, 34.4, 40675, 2214}, -- Chittering Den
+        {49.0, 31.5, 40672, 2214}, -- Gundargaz
+        {57.1, 31.3, 40674, 2214}, -- Lost Mines
+        {62.9, 66.0, 40679, 2214}, -- Taelloch Mine
+        {49.2, 66.2, 40677, 2214}, -- The Living Grotto
+        {56.1, 56.0, 40676, 2214}, -- The Rumbling Wastes
+        {46.9, 10.0, 40673, 2214}, -- The Stonevault Exterior
+        {46.4, 51.6, 40680, 2214} -- The Waterworks
     },
     { -- Hallowfall
-        {62.8, 7.1, 0, 2215}, -- Bleak Sand
-        {69.9, 44.2, 0, 2215}, -- Dunelle's Kindness
-        {30.7, 51.5, 0, 2215}, -- Fortune's Fall
-        {43.3, 52.7, 0, 2215}, -- Mereldar
-        {35.3, 33.9, 0, 2215}, -- Priory of the Sacred Flame
-        {63.7, 65.4, 0, 2215}, -- Sanguine Grasps
-        {57.2, 32.7, 0, 2215}, -- Sina's Yearning
-        {57.6, 64.6, 0, 2215}, -- Tenir's Ascent
-        {62.8, 51.8, 0, 2215}, -- The Fangs
-        {45.7, 12.3, 0, 2215} -- Velhan's Claim
+        {62.8, 7.1, 40685, 2215}, -- Bleak Sand
+        {69.9, 44.2, 40684, 2215}, -- Dunelle's Kindness
+        {30.7, 51.5, 40688, 2215}, -- Fortune's Fall
+        {43.3, 52.7, 40686, 2215}, -- Mereldar
+        {35.3, 33.9, 40687, 2215}, -- Priory of the Sacred Flame
+        {63.7, 65.4, 40683, 2215}, -- Sanguine Grasps
+        {57.2, 32.7, 40682, 2215}, -- Sina's Yearning
+        {57.6, 64.6, 40690, 2215}, -- Tenir's Ascent
+        {62.8, 51.8, 40681, 2215}, -- The Fangs
+        {45.7, 12.3, 40689, 2215} -- Velhan's Claim
     },
     { -- Azj-Kahet
-        {63.4, 13.9, 0, 2255}, -- Arathi's End
-        {58.5, 89.7, 0, 2255}, -- Deepwalker Pass
-        {42.9, 57.1, 0, 2255}, -- Eye of Ansurek
-        {13.2, 34.0, 0, 2213}, -- Old Sacrificial Pit
-        {73.0, 84.7, 0, 2255}, -- Rak-Ush
-        {25.1, 40.8, 0, 2255}, -- Ruptured Lake
-        {46.6, 21.2, 0, 2255}, -- Siegehold
-        {57.5, 57.3, 0, 2255}, -- Silken Ward
-        {66.3, 84.8, 0, 2255}, -- The Maddening Deep
-        {70.5, 25.0, 0, 2255}, -- Trickling Abyss
-        {65.4, 51.7, 0, 2255} -- Untamed Valley
+        {63.4, 13.9, 40691, 2255}, -- Arathi's End
+        {58.5, 89.7, 40696, 2255}, -- Deepwalker Pass
+        {42.9, 57.1, 40694, 2255}, -- Eye of Ansurek
+        {13.2, 34.0, 40695, 2213}, -- Old Sacrificial Pit
+        {73.0, 84.7, 40698, 2255}, -- Rak-Ush
+        {25.1, 40.8, 40693, 2255}, -- Ruptured Lake
+        {46.6, 21.2, 40692, 2255}, -- Siegehold
+        {57.5, 57.3, 40699, 2255}, -- Silken Ward
+        {66.3, 84.8, 40697, 2255}, -- The Maddening Deep
+        {70.5, 25.0, 40700, 2255}, -- Trickling Abyss
+        {65.4, 51.7, 40701, 2255} -- Untamed Valley
     }
 }
 
